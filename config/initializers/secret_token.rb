b55e975f54a90bfe28460b5d39da72e0291bd740
Chorkpop <apedley@gmail.com>
2013-05-20 09:01:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gottadoit::Application.config.secret_token = 'b8c9bb378e1407670be49274258e3045a5765956ddb51e91998582b5939559002cfc2eb10bb3a754f163de5ef0c2d9d53b0f5c3ac33160e778645d99aaed6f3d'
